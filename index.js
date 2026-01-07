611e75b3b67af5f3e8da371d6d5f4a5c506fd929
Presbury Wamae <wamaedev@gmail.com>
2026-01-07 10:53:19 +0300

    Array functions       destructivelyAppendCat(name)         ✓ appends a cat to the end of the cats array       destructivelyPrependCat(name)         ✓ prepends a cat to the beginning of the cats array       destructivelyRemoveLastCat()         ✓ removes the last cat from the cats array       destructivelyRemoveFirstCat()         ✓ removes the first cat from the cats array       appendCat(name)         ✓ appends a cat to the cats array and returns a new array, leaving the cats array unchanged       prependCat(name)         ✓ prepends a cat to the cats array and returns a new array, leaving the cats array unchanged       removeLastCat()         ✓ removes the last cat in the cats array and returns a new array, leaving the cats array unchanged       removeFirstCat()         ✓ removes the first cat from the cats array and returns a new array, leaving the cats array unchanged

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 // Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"];
+let cats = ["Milo", "Otis", "Garfield"];
 
+function destructivelyAppendCat(name){
+  cats.push(name);
+  return cats;
+}
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name){
+  cats.unshift(name);
+  return cats;
+}
+destructivelyPrependCat("Bob");
+
+function destructivelyRemoveLastCat(name){
+  cats.pop(name);
+  return cats;
+}
+destructivelyRemoveLastCat()
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+  return cats;
+}
+destructivelyRemoveFirstCat();
+
+function appendCat(name){
+  let appendedCats = [...cats, name];
+  return appendedCats;
+}
+appendCat("George");
+
+function prependCat(name){
+  let appendedCats = [name,...cats]
+  return appendedCats
+}
+prependCat("Scott");
+
+function removeLastCat(){
+  let newCats = cats.slice(0, -1);
+  return newCats;
+}
+removeLastCat()
+
+function removeFirstCat(){
+  let newCats = cats.slice(1);
+  return newCats;
+}
+removeFirstCat();
